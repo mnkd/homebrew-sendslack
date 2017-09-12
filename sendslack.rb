010e05a6a43f05cf244923ada65c13bc0e6b9c35
Mitsuru Nakada <m-nakada@nekojarashi.com>
2017-09-12 09:33:23 +0900

    :tada: :up: v0.2.0

--- sendslack.rb
+++ sendslack.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Sendslack < Formula
   homepage "https://github.com/mnkd/sendslack"
-  url "https://github.com/mnkd/sendslack/releases/download/v0.1.0/sendslack_v0.1.0.tar.gz"
-  sha256 "2422b757088207fa083fc1a35e86c25e9c3b9d1d9e1fa16998db182702cbcd90"
+  url "https://github.com/mnkd/sendslack/releases/download/v0.2.0/sendslack_v0.2.0.tar.gz"
+  sha256 "d357dd89e6d2e75e7435aaf0071931f8712534f24889ca28758b00bcf0f6b1d3"
   head "https://github.com/mnkd/sendslack.git"
-  version "0.1.0"
+  version "0.2.0"
 
   def install
     bin.install "sendslack"
